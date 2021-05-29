917274ac36c0199248b63ea772b7e3b79b591917
Sifatul <sifii2013@gmail.com>
2021-05-29 22:27:46 +0900

    admin message listener

--- src/admin/index.tsx
+++ src/admin/index.tsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react';
+import {firebaseDatabase} from "../FirebaseSettings"
+import ReactFirebaseChatMain from "../Component/index"
+import {messageBody} from "../Interfaces/message.enum"
 
-export default function index() {
+export default function Index() {
+    useEffect(()=>{
+        const reactFirebaseChat:any = new ReactFirebaseChatMain(firebaseDatabase)
+        reactFirebaseChat.clientsMessageListener((clientMsgs:messageBody)=>{
+            console.log(clientMsgs)
+        })
+        
+    },[])
     return (
         <div>
             Admin
