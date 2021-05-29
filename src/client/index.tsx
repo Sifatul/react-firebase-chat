859ed56b9855557a77219baa909ef05f76457c72
Sifatul <sifii2013@gmail.com>
2021-05-29 20:47:43 +0900

    listeners

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,6 +1,22 @@
 import React from 'react'
+import ReactFirebaseChatMain from "../component/index"
+import {firebaseDatabase} from "../FirebaseSettings"
 
 export default function index() {
+    const reactFirebaseChatMain:any = new ReactFirebaseChatMain(firebaseDatabase)
+    reactFirebaseChatMain.addClientMessage({
+        message: "message",
+        name: "name",
+        userType: "client",
+        readStatus: "false",
+    },()=>{})
+    reactFirebaseChatMain.clientsMessageListener((data:[])=>{
+        console.log("clients:", data)
+    })
+    const clientId = '08e47862-7edf-4613-abf0-6a506bfff54f'
+    reactFirebaseChatMain.clientMessageListenerClientById(clientId, (data:[])=>{
+        console.log("client message :", data)
+    })
     return (
         <div>
             Client
