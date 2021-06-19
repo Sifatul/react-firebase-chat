3884113da06ea7b9f71710aea543d3181e5bd353
Sifatul <sifii2013@gmail.com>
2021-06-19 21:51:54 +0900

    sernderUid added

--- src/FirebaseModule/Common/InputPopUP.tsx
+++ src/FirebaseModule/Common/InputPopUP.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"; 
 // import Styles from './InputPopUp.module.css';
-import { addMessage } from "../FirebaseLib/index"
+import { addMessage, messageListenerById } from "../FirebaseLib/index"
+import { messageBody } from "../FirebaseLib/messageInterface"
 
 const Styles: {
     "inputPopUpWrpper": React.CSSProperties,
@@ -56,10 +57,28 @@ const Styles: {
 
 }
 
+ 
 
 export default function InputPopUP(props: any) {
     const [name, setName] = React.useState("Bob Doe");
     const [message, setMessage] = React.useState("Hello There!")
+    const [messages, setMessages ] = React.useState<messageBody[]>([])
+    const {Uid, firebaseDatabase} = props
+
+    const messageListenCallbackFunc= (data :any)=>{
+        console.log("data received: ",data)
+        const output = []
+        for(const i in data){
+            output.push(data[i])
+        }
+        setMessages(output)
+      }
+      const errorFunction= (error :any)=>{
+        console.log("error occured: ",error)
+      }
+      React.useEffect(()=>{
+        messageListenerById({firebaseDatabase: props.firebaseDatabase , callback: messageListenCallbackFunc, Uid: props.Uid, errorCallback : errorFunction })
+      },[])
 
 
     function sendMessage() {
@@ -70,22 +89,30 @@ export default function InputPopUP(props: any) {
                 name: "name",
                 userType: "client",
                 readStatus: false,
+                senderUid: Uid
             },
-            firebaseDatabase: props.firebaseDatabase,
+            firebaseDatabase: firebaseDatabase,
             callback: () => { },
-            Uid: props.Uid
+            Uid: Uid
         }
         )
     }
 
     return (
         <div style={Styles.inputPopUpWrpper}>
-            <input
+            {/* <input
                 style={{ ...Styles.inputPopUpWrpperInputName, ...Styles.fWidth }}
                 placeholder={name}
                 value={name}
                 onChange={e => setName(e.target.value)}
-            />
+            /> */}
+            {messages.map(item=>{
+                if(item.senderUid === Uid){
+                    return <p>{item.message}</p>
+                }
+                return <p>{item.message}</p>
+            })}
+            
             <textarea
                 style={{ ...Styles.inputPopUpWrpperInputMessage, ...Styles.fWidth }}
                 placeholder={message}
